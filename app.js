26d43d578f9c2b06e85200e358caa7d8f0109d33
ბაქარი <57666394+xajiraqab@users.noreply.github.com>
2021-03-28 19:09:17 +0400

    დასაწყისი :3

---
+++ app.js
@@ -0,0 +1,91 @@
+const path = require("path");
+var express = require('express')
+var app = express()
+const fs = require('fs')
+const http = require('http')
+const axios = require('axios');
+const UrlPattern = require('url-pattern')
+
+const sendResponse = (res, seo = null) => {
+    fs.readFile(path.join(__dirname, 'public', 'app.html'), 'utf8', (err, data) => {
+
+        if (err) {
+            res.status(404).send('შეცდომა :3');
+            return
+        }
+
+
+        let title = "AiaSoft"
+        let description = "პროგრამირების გაკვეთილებისა და ამოცანების ვებ-გვერდი"
+
+        let og = {
+            title: title,
+            description: description
+        }
+
+        if (seo) {
+            title = seo.title
+            description = seo.description
+            og.title = seo.og.title
+            og.description = seo.og.description
+        //    console.log(seo)
+        }
+
+        res.send(data.replace('%TITLE', title).replace('%DESCRIPTION', description).replace('%OG:TITLE', og.title).replace('%OG:DESCRIPTION', og.description))
+    })
+}
+
+async function getSEO_fullProblem(res, url) {
+
+    try {
+
+        const response = await axios.get(`http://aiasoft.ge:5000${url}`)
+
+        if (response.status !== 200) {
+            sendResponse(res)
+            return
+        }
+
+        let data = response.data
+        sendResponse(res, {
+            title: data.id + '. ' + data.title,
+            description: data.statement,
+            og: {
+                title: data.id + '. ' + data.title,
+                description: data.statement
+            }
+         })
+
+
+    } catch (error) {
+        sendResponse(res)
+
+    }
+}
+
+
+
+app.use(express.static('public'))
+
+app.use((req, res) => {
+
+
+    let url = req.url
+
+    let isFullProblem = new UrlPattern('/problem/:id').match(url)
+
+    if (isFullProblem) {
+        getSEO_fullProblem(res, url)
+        return
+    }
+
+    sendResponse(res)
+
+})
+
+
+
+
+
+
+app.listen(4000)
---
+++ public/precache-manifest.6fbe007d201e0447011d36a5b84d1812.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a43ce523c497e5764a201caca6f8d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71b7f65c71ec9b5e6be3",
+    "url": "/static/css/2.1e0f4af7.chunk.css"
+  },
+  {
+    "revision": "6578f5727a5f702ec3a8",
+    "url": "/static/css/main.65d4d01e.chunk.css"
+  },
+  {
+    "revision": "71b7f65c71ec9b5e6be3",
+    "url": "/static/js/2.7d604fc5.chunk.js"
+  },
+  {
+    "revision": "28b06029996ae998bfc1",
+    "url": "/static/js/3.7b5a3e94.chunk.js"
+  },
+  {
+    "revision": "6578f5727a5f702ec3a8",
+    "url": "/static/js/main.125f6ebe.chunk.js"
+  },
+  {
+    "revision": "6d449ec5e9b324f83076",
+    "url": "/static/js/runtime-main.f9751d00.js"
+  },
+  {
+    "revision": "fa6d3cc99729402dbb2664cff3484e59",
+    "url": "/static/media/BPG_Arial_2018.fa6d3cc9.ttf"
+  },
+  {
+    "revision": "e331fd9a0e30bb90942cfdc157a3f831",
+    "url": "/static/media/BPG_Arial_Caps_Web_2018.e331fd9a.ttf"
+  },
+  {
+    "revision": "6695af9986412e75985538255ca87866",
+    "url": "/static/media/p404.6695af99.svg"
+  }
+]);
